1608589a6541639e3e2c8674b830deb5a2e12e89
mrkhudaykulov <mr.khudaykulov@gmail.com>
2026-06-14 13:10:16 +0500

    Update states.py

--- states.py
+++ states.py
@@ -35,3 +35,8 @@ class PriceInputStates(StatesGroup):
     animal_type = State()
     region = State()
     price = State()
+
+class PriceFromIndexStates(StatesGroup):
+    """Нархлар индексидан нарх киритиш"""
+    region = State()
+    price = State()
