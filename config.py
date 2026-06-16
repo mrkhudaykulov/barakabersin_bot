051936b30f8402d16a1e542d1a9a20223bfda24e
mrkhudaykulov <mr.khudaykulov@gmail.com>
2026-06-16 14:51:10 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,13 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = -1001419724490
+
+# Админ ID лари — бир неча одам бўлиши mumkin
 ADMINS = [72185847, 2134695872]
+
+# Тасдиқловчи админлар (шулардан биттаси тасдиқлайди)
+REVIEW_ADMINS = [72185847, 2134695872]
+
 DATABASE_URL = os.getenv("DATABASE_URL") # PostgreSQL yoki SQLite
 
 bot = Bot(token=BOT_TOKEN)
